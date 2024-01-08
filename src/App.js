976a641fb2c115634bd32f3b0ede1abcba4ce91d
mehmet-akif <makif8992@gmail.com>
2024-01-08 18:52:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.css'; // Import the CSS file
 
-function App() {
+const UserList = () => {
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        if (!response.ok) {
+          throw new Error('Network response was not ok.');
+        }
+        const data = await response.json();
+        setUsersData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-table-container">
+      <h1 class="centertext" >User List</h1>
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Website</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersData.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.website}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
-export default App;
+export default UserList;
